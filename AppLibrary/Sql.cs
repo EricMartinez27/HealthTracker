cdb2f6c3ffcbcd5b4a572907c9aa7c7a52e473cd
LTI <LTI@INFEDL31041.lntinfotech.com>
2021-09-27 13:59:43 -0500

    added user interface

--- AppLibrary/Sql.cs
+++ AppLibrary/Sql.cs
@@ -3,73 +3,82 @@ using Microsoft.Data.Sqlite;
 
 namespace AppLibrary
 {
-  public class Sql
-  {
-    public static void Query()
+    public class Sql
     {
-      using var connection = new SqliteConnection("Data Source=./AppLibrary/data/healthtracking.db");
-      connection.Open();
-      var command = connection.CreateCommand();
-      command.CommandText =
-      @"
+        public static void Query()
+        {
+            using var connection = new SqliteConnection("Data Source=./AppLibrary/data/healthtracking.db");
+            connection.Open();
+            var command = connection.CreateCommand();
+            command.CommandText =
+            @"
         select *
         from dailyProgress
       ";
-      using var reader = command.ExecuteReader();
-      while (reader.Read())
-      {
-        var day = reader.GetInt32(0);
-        var weight = reader.GetInt32(1);
-        var bmi = reader.GetFloat(2);
-        var calories = reader.GetInt32(3);
-        Console.WriteLine($"Day : {day}, {weight}, {bmi}, {calories}");
-      }
-    }
+            using var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                var day = reader.GetInt32(0);
+                var weight = reader.GetInt32(1);
+                var bmi = reader.GetDouble(2);
+                var calories = reader.GetInt32(3);
+                Console.WriteLine($"Day: {day}, Weight: {weight}, BMI: {bmi}, Calories: {calories}");
+            }
+        }
 
-    public static void Update()
-    {
-       int userDay = 4;
-       int userWeight = 175;
-       float userBmi = 20.5F;
-       int userCalories = 3000;
+        public static void Update()
+        {
+            int userDay;
+            int userWeight;
+            double userBmi;
+            int userCalories;
+            Console.WriteLine("Please enter the day you want to update:");
+            userDay = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the weight you want to update:");
+            userWeight = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the BMI you want to update:");
+            userBmi = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please enter the calories you want to update:");
+            userCalories = Convert.ToInt32(Console.ReadLine());
 
-      using var connection = new SqliteConnection("Data Source=./AppLibrary/data/healthtracking.db");
-      connection.Open();
-      var command = connection.CreateCommand();
-      command.CommandText = 
-      @"
+            using var connection = new SqliteConnection("Data Source=./AppLibrary/data/healthtracking.db");
+            connection.Open();
+            var command = connection.CreateCommand();
+            command.CommandText =
+            @"
         UPDATE dailyProgress
         SET weight = $newweight, bmi = $newbmi, calories = $newcalories
         WHERE day = $day;
         ";
 
-        command.Parameters.AddWithValue("$day", userDay);
-        command.Parameters.AddWithValue("$newweight", userWeight);
-        command.Parameters.AddWithValue("$newbmi", userBmi);
-        command.Parameters.AddWithValue("$newcalories", userCalories);
+            command.Parameters.AddWithValue("$day", userDay);
+            command.Parameters.AddWithValue("$newweight", userWeight);
+            command.Parameters.AddWithValue("$newbmi", userBmi);
+            command.Parameters.AddWithValue("$newcalories", userCalories);
 
-        using var reader = command.ExecuteReader();
-    }
+            using var reader = command.ExecuteReader();
+        }
 
-    public static void Delete()
-    {
-       int userDay = 7;
-     
-      using var connection = new SqliteConnection("Data Source=./AppLibrary/data/healthtracking.db");
-      connection.Open();
-      var command = connection.CreateCommand();
-      command.CommandText = 
-      @"
+        public static void Delete()
+        {
+            int userDay;
+            Console.WriteLine("Please enter the day you want to delete:");
+            userDay = Convert.ToInt32(Console.ReadLine());
+            using var connection = new SqliteConnection("Data Source=./AppLibrary/data/healthtracking.db");
+            connection.Open();
+            var command = connection.CreateCommand();
+            command.CommandText =
+            @"
         DELETE
         FROM dailyProgress
         WHERE day = $day;
         ";
 
-        command.Parameters.AddWithValue("$day", userDay);
+            command.Parameters.AddWithValue("$day", userDay);
 
-        using var reader = command.ExecuteReader();
-  }
-  }
+            using var reader = command.ExecuteReader();
+        }
+    }
 }
 
 
--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -7,10 +7,40 @@ namespace MainApp
     {
         static void Main(string[] args)
         {
-           Sql.Update();
-           Sql.Delete();
-           Sql.Query();
+            bool isRunning = true;
 
+            Console.WriteLine("Welcome to your health tracker!");
+            while (isRunning)
+            {
+                isRunning = UserInterface();
+            }
+
+        }
+        static bool UserInterface()
+        {
+            Console.WriteLine("What would you like to do? Make a selection: Enter 1 to Update Enter 2 to Delete Enter 3 to View Results Enter 0 to Quit");
+
+            var userInput = Console.ReadLine();
+
+            switch (userInput)
+            {
+                case "0":
+                    return false;
+                case "1":
+                    Sql.Update();
+                    break;
+                case "2":
+                    Sql.Delete();
+                    break;
+                case "3":
+                    Sql.Query();
+                    break;
+                default:
+                    Console.WriteLine("Not a valid option");
+                    break;
+
+            }
+            return true;
         }
     }
 }
